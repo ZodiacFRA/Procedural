0d3e6a0632c6a5993affc89831dd76eb3fd98f37
jb <jean-baptiste.dupuy@epitech.eu>
2019-11-03 19:46:01 +0000

    cube gen working

--- imGenerator.py
+++ imGenerator.py
@@ -1,8 +1,7 @@
-from PIL import Image
 import copy
+import random
 
-IMG_WIDTH = 2
-# IMG_HEIGHT = 512
+from PIL import Image
 
 
 class Color(object):
@@ -14,50 +13,67 @@ class Color(object):
     def __repr__(self):
         return f"({self.r}, {self.g}, {self.b})"
 
-
-def create_cube(size):
-    tmp_buffer = [255] * 3 * size ** 2
-    fill_ring(tmp_buffer, size, 0, [255, 0, 0])
-    return tmp_buffer
+    def get_list(self):
+        return [self.r, self.g, self.b]
 
 
-def fill_ring(tmp_buffer, cube_size, ring_idx, color):
+def init_img_buffer(color):
+    img_buffer, line = [], []
+    for i in range(IMG_SIZE):
+        line.append(copy.deepcopy(color))
+    for i in range(IMG_SIZE):
+        img_buffer.append(copy.deepcopy(line))
+    return img_buffer
+
+
+def prepare_img_buffer(img_buffer):
+    res = []
+    for x in range(IMG_SIZE):
+        for y in range(IMG_SIZE):
+            res += img_buffer[x][y].get_list()
+    return bytes(res)
+
+
+def create_cube(img_buffer, size, x_start, y_start, palette):
+    fill_ring(img_buffer, size, x_start, y_start, 0, random.choice(palette))
+
+
+def print_img_buffer(img_buffer):
+    for line in img_buffer:
+        print(line)
+
+
+def fill_ring(img_buffer, size,  x_start, y_start, ring_idx, color):
     """ring_idx start at 0 (outside) and finish at cube_size - 1 (center)"""
-    # Top line
-    for x in range(ring_idx, cube_size, 3):
-        tmp_buffer[ring_idx][x + 0] = 12# color[0]
-        tmp_buffer[ring_idx][x + 1] = color[1]
-        tmp_buffer[ring_idx][x + 2] = color[2]
-    # Bottom line
-    for x in range(ring_idx, cube_size, 3):
-        tmp_buffer[cube_size - ring_idx][x + 0] = color[0]
-        tmp_buffer[cube_size - ring_idx][x + 1] = color[1]
-        tmp_buffer[cube_size - ring_idx][x + 2] = color[2]
+    y = y_start + ring_idx
+    final_x_start = x_start + ring_idx
+    final_x_end = final_x_start + size
+    final_y_start = y_start + ring_idx
+    final_y_end = final_y_start + size
+
+    # Horizontal
+    for x in range(final_x_start, final_x_end + 1):
+        img_buffer[final_y_start][x] = color
+        img_buffer[final_y_end][x] = color
+    # Vertical
+    for y in range(final_y_start, final_y_end + 1):
+        img_buffer[y][final_x_start] = color
+        img_buffer[y][final_x_end] = color
 
 
 if __name__ == '__main__':
-    img_buffer = []
-    line = []
-    for i in range(IMG_WIDTH):
-        line.append(Color())
-    for i in range(IMG_WIDTH):
-        img_buffer.append(copy.deepcopy(line))
+    IMG_SIZE = 16
+    black = Color(0, 0, 0)
+    white = Color(255, 255, 255)
+    red = Color(255, 0, 0)
+    palette = [black, white, red]
 
-    for line in img_buffer:
-        print(line)
-    print("-"*10)
-    img_buffer[0][0].r = 0
-    for line in img_buffer:
-        print(line)
+    img_buffer = init_img_buffer(palette.pop(random.randint(0, len(palette) - 1)))
+    cube_size = 4
+    for x in range(0, IMG_SIZE, cube_size):
+        for y in range(0, IMG_SIZE, cube_size):
+            create_cube(img_buffer, cube_size - 1, x, y, palette)
 
-
-    exit()
-    cube_size = 16
-    tmp_cube = create_cube(cube_size*3)
-    for x in range(cube_size*3):
-        for y in range(cube_size*3):
-            img_buffer[x][y] = tmp_cube[x][y]
-
-
-    im = Image.frombytes("RGB", (IMG_WIDTH, IMG_WIDTH), bytes(img_buffer))
+    # img_buffer is a list
+    im = Image.frombytes("RGB", (IMG_SIZE, IMG_SIZE), prepare_img_buffer(img_buffer))
     im.save("./test.png", "PNG")
