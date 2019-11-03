189582383384607a56abe3608aeae968d028c881
jb <jean-baptiste.dupuy@epitech.eu>
2019-11-03 20:35:32 +0000

    added random palette loading

--- imGenerator.py
+++ imGenerator.py
@@ -1,5 +1,7 @@
 import copy
 import random
+from os import listdir
+from os.path import isfile, join
 
 from PIL import Image
 
@@ -34,11 +36,6 @@ def prepare_img_buffer(img_buffer):
     return bytes(res)
 
 
-def create_cube(img_buffer, size, x_start, y_start, palette):
-    for ring_idx in range(size//2):
-        fill_ring(img_buffer, size, x_start, y_start, ring_idx, random.choice(palette))
-
-
 def print_img_buffer(img_buffer):
     for line in img_buffer:
         print(line)
@@ -47,6 +44,11 @@ def print_img_buffer(img_buffer):
 def get_rgb_from_hex(hex_code):
     res = tuple(int(hex_code[i:i + 2], 16) for i in (0, 2, 4))
     return res
+
+
+def load_random_palette(dirpath):
+    onlyfiles = [f for f in listdir(dirpath) if isfile(join(dirpath, f))]
+    return load_palette_from_file("./palettes/" + random.choice(onlyfiles))
 
 
 def load_palette_from_file(filepath):
@@ -62,6 +64,11 @@ def load_palette_from_file(filepath):
     return palette
 
 
+def create_cube(img_buffer, size, x_start, y_start, palette):
+    for ring_idx in range(size//2):
+        fill_ring(img_buffer, size, x_start, y_start, ring_idx, random.choice(palette))
+
+
 def fill_ring(img_buffer, size,  x_start, y_start, ring_idx, color):
     """ring_idx start at 0 (outside) and finish at cube_size - 1 (center)"""
     y = y_start + ring_idx
@@ -69,7 +76,7 @@ def fill_ring(img_buffer, size,  x_start, y_start, ring_idx, color):
     final_x_end = x_start + size - ring_idx
     final_y_start = y_start + ring_idx
     final_y_end = y_start + size - ring_idx
-    # print(f"{'-'*10}\n{final_x_start}/{final_y_start}, {final_x_end}/{final_y_end}")
+    # print(f"{'-'*10}\n{final_x_start}/{final_y_start}, {final_x_end}/{final_y_end}", flush=True)
 
     # Horizontal
     for x in range(final_x_start, final_x_end + 1):
@@ -82,15 +89,16 @@ def fill_ring(img_buffer, size,  x_start, y_start, ring_idx, color):
 
 
 if __name__ == '__main__':
-    IMG_SIZE = 512
-    palette = load_palette_from_file("./palette.cfg")
+    IMG_SIZE = 128
+    # palette = load_palette_from_file("./palettes/circus_III.palette")
+    palette = load_random_palette("./palettes")
     bg_color = palette.pop(random.randint(0, len(palette) - 1))
     img_buffer = init_img_buffer(bg_color)
-    cube_size = 8
+    cube_size = 16
     for x in range(0, IMG_SIZE, cube_size):
         for y in range(0, IMG_SIZE, cube_size):
             create_cube(img_buffer, cube_size - 1, x, y, palette)
 
     # img_buffer is a list
     im = Image.frombytes("RGB", (IMG_SIZE, IMG_SIZE), prepare_img_buffer(img_buffer))
-    im.save("./test.png", "PNG")
+    im.save("./example.png", "PNG")
